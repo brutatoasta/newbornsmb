c0f3a402649153356cdd6b0eb1a79718a0a4e182
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2023-10-02 10:20:14 +0800

    add stomp sound make goomba a prefab

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -7,12 +7,15 @@ public class PlayerCombat : MonoBehaviour
 {
     public EnemyMovement enemyMovement;
     public PlayerMovement playerMovement;
+
+    public AudioSource audioSource;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemies") && playerMovement.alive && enemyMovement.alive)
         {
             Debug.Log("Collided with goomba!");
             enemyMovement.Die();
+            audioSource.PlayOneShot(audioSource.clip);
 
         }
 
