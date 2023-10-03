592c4e02913acd146384951ea519d901ef1b4308
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2023-10-03 22:15:47 +0800

    fix: 2nd goomba refrags first goomba

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -11,9 +11,11 @@ public class PlayerCombat : MonoBehaviour
     public AudioSource audioSource;
     void OnTriggerEnter2D(Collider2D other)
     {
+        enemyMovement = other.gameObject.GetComponent<EnemyMovement>();
         if (other.gameObject.CompareTag("Enemies") && playerMovement.alive && enemyMovement.alive)
         {
             Debug.Log("Collided with goomba!");
+
             enemyMovement.Die();
             audioSource.PlayOneShot(audioSource.clip);
 
